ec8d9589a52fe2f4c052a45d6211235be13517c9
MIGUEL PEREIRA ARAUJO <miguel.araujo25@senaisp.edu.br>
2026-03-13 10:54:47 -0300

    Atividade 1

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-let nomeCliente = document.getElementById("nomeCliente");
-let btnAlterar = document.querySelector("#btnAlterar");
-let mensagem = document.getElementById("mensagem");
-let nomeAlterado = document.querySelector("#nomeAlterado")
+let nomeCadastro = document.getElementById("nomeCadastro");
+let btnCadastrar = document.querySelector("#btnCadastrar");
 
-btnAlterar.addEventListener("click", ()=>{
-
-    nomeCliente.innerText = `Olá ${nomeAlterado.value}, como vai?`;
-
-    mensagem.innerText = `Alteração feita com sucesso!`
-    mensagem.className = 'text-success fw-bold'
+btnCadastrar.addEventListener("click", ()=>{
+   
+    alert(`Bem vindo: ${nomeCadastro.value}!`)
 
 });
