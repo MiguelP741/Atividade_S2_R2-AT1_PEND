5d6ca92c4ca2a8d5befbca8d9318e4a75bef1e5f
MIGUEL PEREIRA ARAUJO <miguel.araujo25@senaisp.edu.br>
2026-03-13 09:31:06 -0300

    Atividade 1

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let nomeCadastro = document.getElementById("nomeCadastro");
 let btnCadastrar = document.querySelector("#btnCadastrar");
+let mensagem = document.getElementById("mensagem");
 
 btnCadastrar.addEventListener("click", ()=>{
-   
-    alert(`Bem vindo: ${nomeCadastro.value}!`)
+
+    mensagem.innerText = `Bem vindo: ${nomeCadastro.value}!`
+    mensagem.className = 'text-success fw-bold'
 
 });
